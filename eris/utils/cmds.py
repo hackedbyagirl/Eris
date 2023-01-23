0849e0136868f7f42b01c44ccc90a71f7aa5753c
bdawg <>
2023-01-23 14:41:08 -0700

    fix syntax errors

--- eris/utils/cmds.py
+++ eris/utils/cmds.py
@@ -11,65 +11,75 @@ class CmdTest:
         pass
 
     def execute_cmd(self, cmd, cwd=None):
-        cmd_str = ' '.join(cmd)
+        cmd_str = " ".join(cmd)
         msg = "{G} Executing: {C} " + cmd_str
-            
+
         try:
             Color.print(msg)
             output = s.check_output(cmd, cwd=cwd, shell=False)
             return output.decode()
         except s.CalledProcessError as e:
             return e.output.decode()
-            
 
 
-class Command():
-
+class Command:
     @staticmethod
     def devnull():
-        ''' Helper method for opening devnull '''
-        return open('/dev/null', 'w')
-    
+        """Helper method for opening devnull"""
+        return open("/dev/null", "w")
+
     @staticmethod
     def packer(cmd, pwd):
-        
 
-        if type(stdout) is bytes: stdout = stdout.decode('utf-8')
-        if type(stderr) is bytes: stderr = stderr.decode('utf-8')
+        if type(stdout) is bytes:
+            stdout = stdout.decode("utf-8")
+        if type(stderr) is bytes:
+            stderr = stderr.decode("utf-8")
 
-        if Config.verbose > 1 and stdout is not None and stdout.strip() != '':
-            Color.pe('{P} [stdout] %s{W}' % '\n [stdout] '.join(stdout.strip().split('\n')))
-        if Config.verbose > 1 and stderr is not None and stderr.strip() != '':
-            Color.pe('{P} [stderr] %s{W}' % '\n [stderr] '.join(stderr.strip().split('\n')))
+        if Config.verbose > 1 and stdout is not None and stdout.strip() != "":
+            Color.pe(
+                "{P} [stdout] %s{W}" % "\n [stdout] ".join(stdout.strip().split("\n"))
+            )
+        if Config.verbose > 1 and stderr is not None and stderr.strip() != "":
+            Color.pe(
+                "{P} [stderr] %s{W}" % "\n [stderr] ".join(stderr.strip().split("\n"))
+            )
 
         return (stdout, stderr)
-    
+
     @staticmethod
     def program_exists(program):
-        ''' Checks if program is installed on this system '''
-        command = ['which', program]
-        
+        """Checks if program is installed on this system"""
+        command = ["which", program]
+
         proc = Command(command)
 
         stdout = proc.stdout().strip()
         stderr = proc.stderr().strip()
 
-        if stdout == '' and stderr == '':
+        if stdout == "" and stderr == "":
             return False
 
-        return True 
-
+        return True
 
     def launch(self, path, stdout=s.PIPE, stderr=s.PIPE):
-        ''' Launch a tool from a specific directory'''
-        print('Launching command')
+        """Launch a tool from a specific directory"""
+        print("Launching command")
         proc = s.Popen(self.cmd, cwd=path, stdin=s.PIPE, stdout=stdout, stderr=stderr)
         out, err = proc.communicate(path)
-        return(out, err)
+        return (out, err)
 
-
-    def __init__(self, cmd='', devnull=False, stdout=s.PIPE, stderr=s.PIPE, cwd=None, bufsize=0, stdin=s.PIPE):
-        '''cmd = command line program with arguments in a list format. Must be in list format to have shell = false'''
+    def __init__(
+        self,
+        cmd="",
+        devnull=False,
+        stdout=s.PIPE,
+        stderr=s.PIPE,
+        cwd=None,
+        bufsize=0,
+        stdin=s.PIPE,
+    ):
+        """cmd = command line program with arguments in a list format. Must be in list format to have shell = false"""
 
         if type(cmd) is str:
             self.cmd = cmd.split()
@@ -77,26 +87,28 @@ class Command():
             self.cmd = cmd
 
         self.output = []
-        
-       # self.out = None
-       # self.err = None
 
-       # if devnull:
-       #     std_out = Command.devnull()
-       #     std_err = Command.devnull()
-       # else:
-       #     std_out = stdout
-       #     std_err = stderr
+    # self.out = None
+    # self.err = None
 
-        #self.proc = s.Popen(cmd, stdout=std_out, stderr=std_err, stdin=stdin, cwd=cwd, bufsize=bufsize)      
+    # if devnull:
+    #     std_out = Command.devnull()
+    #     std_err = Command.devnull()
+    # else:
+    #     std_out = stdout
+    #     std_err = stderr
+
+    # self.proc = s.Popen(cmd, stdout=std_out, stderr=std_err, stdin=stdin, cwd=cwd, bufsize=bufsize)
 
     def run(self, shell=False, universal_newlines=True):
-        '''
+        """
         Takes in a command prepopulated with arguments and runs it. The command MUST be in a list format'
-        '''
+        """
 
         try:
-            self.output = s.check_output(self.cmd, shell=False, stderr=s.STDOUT, universal_newlines=True)
+            self.output = s.check_output(
+                self.cmd, shell=False, stderr=s.STDOUT, universal_newlines=True
+            )
 
         except s.CalledProcessError as cp:
             return False
@@ -105,27 +117,26 @@ class Command():
             return False
 
         return self
-    
-    
+
     def stdout(self):
-        ''' Waits for process to finish, returns stdout output '''
+        """Waits for process to finish, returns stdout output"""
         self.get_output()
         return self.out
 
     def stderr(self):
-        ''' Waits for process to finish, returns stderr output '''
+        """Waits for process to finish, returns stderr output"""
         self.get_output()
         return self.err
 
     def get_output(self):
-        ''' Waits for process to finish, sets stdout & stderr '''
+        """Waits for process to finish, sets stdout & stderr"""
         if self.out is None:
             (self.out, self.err) = self.proc.communicate()
 
         if type(self.out) is bytes:
-            self.out = self.out.decode('utf-8')
+            self.out = self.out.decode("utf-8")
 
         if type(self.err) is bytes:
-            self.err = self.err.decode('utf-8')
+            self.err = self.err.decode("utf-8")
 
         return (self.out, self.err)
--- eris/utils/colors.py
+++ eris/utils/colors.py
@@ -6,9 +6,9 @@ import re
 
 class Color(object):
 
-    ''' Helpers for printing to terminal '''
-    
-    '''
+    """Helpers for printing to terminal"""
+
+    """
     Color Keys:
     red = 1
     orange = 214
@@ -32,43 +32,49 @@ class Color(object):
     212 = nice purple
     171 = purple
     141 = cool purple
-    '''
+    """
     colors = {
-        'W' : 15,  # white (normal)
-        'R' : 1, 
-        'G' : 2,
-        'O' : 214,
-        'B' : 4,
-        'P' : 141,
-        'C' : 81,
-        'GR': 245
+        "W": 15,  # white (normal)
+        "R": 1,
+        "G": 2,
+        "O": 214,
+        "B": 4,
+        "P": 141,
+        "C": 81,
+        "GR": 245,
     }
 
     last_sameline_length = 0
-    
+
     @staticmethod
     def s(text):
         base = text
-        output = ''
-        
+        output = ""
+
         matches = re.findall(r"\{([A-Z])\}", text)
         for match in matches:
             color_key = match
             color_code = Color.colors[color_key]
-            base = base.replace("{" + color_key + "}", '')
+            base = base.replace("{" + color_key + "}", "")
             test = stylize(base, fg(color_code))
-        output = ''.join([output, test])
+        output = "".join([output, test])
 
         return output
-            
-       
 
     @staticmethod
     def print(text):
         output = Color.s(text)
-        print(output)    
-        
-        
+        print(output)
+
+    @staticmethod
+    def p(text, color, attrib=None):
+        """
+        Prints text using colored format on same line.
+        Usage = Color.p('Text.', 'Color Code', Style code)
+        """
+        output = Color.set(text, color, attrib)
+        print(output)
+
     @staticmethod
     def set(text, color, attrib=None):
         r = 1
@@ -81,33 +87,32 @@ class Color(object):
         gr = 245
         wh = 15
 
-        if color == 'r':
+        if color == "r":
             color_code = r
 
-        if color == 'orng':
+        if color == "orng":
             color_code = orng
 
-        if color == 'y':
+        if color == "y":
             color_code = y
 
-        if color == 'g':
+        if color == "g":
             color_code = g
 
-        if color == 'b':
+        if color == "b":
             color_code = b
 
-        if color == 'cy':
+        if color == "cy":
             color_code = cy
 
-        if color == 'purp':
+        if color == "purp":
             color_code = purp
 
-        if color == 'gr':
+        if color == "gr":
             color_code = gr
 
-        if color == 'wh':
+        if color == "wh":
             color_code = wh
-
 
         if attrib is None:
             output = stylize(text, fg(color_code))
@@ -115,13 +120,4 @@ class Color(object):
             style = fg(color_code) + attr(attrib)
             output = stylize(text, style)
 
-        return output    
-
-    @staticmethod
-    def p(text, color, attrib=None):
-        '''
-        Prints text using colored format on same line.
-        Usage = Color.p('Text.', 'Color Code', Style code)
-        '''
-        output = Color.set(text, color, attrib)
-        print(output)
+        return output
