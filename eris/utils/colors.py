986499760f888b6ec376f7512a2e6599b8e854ad
bdawg <>
2023-01-23 11:18:34 -0700

    update colors

--- eris/utils/colors.py
+++ eris/utils/colors.py
@@ -1,131 +1,127 @@
 #!/usr/bin/env python3
 
-import subprocess as s
-
-from ..config import Config
-from .colors import Color
+from colored import fg, attr, stylize
+import re
 
 
-class CmdTest:
-    def __init__(self):
-        pass
+class Color(object):
 
-    def execute_cmd(self, cmd, cwd=None):
-        cmd_str = ' '.join(cmd)
-        msg = "{G} Executing: {C} " + cmd_str
-            
-        try:
-            Color.print(msg)
-            output = s.check_output(cmd, cwd=cwd, shell=False)
-            return output.decode()
-        except s.CalledProcessError as e:
-            return e.output.decode()
-            
+    ''' Helpers for printing to terminal '''
+    
+    '''
+    Color Keys:
+    red = 1
+    orange = 214
+    yellow = 220
+    green = 2
+    blue = 4
+    cyan = 81
+    purple = 141
+    gray = 245
+    white = 15
+    
+    Attributes keys:
+    0 = reset
+    1 = bold
+    2 = dim
+    21 = reset bold
+    22 = rest dim
+    Extra colors:
+    33 = dog blue
+    226 = bright yellow
+    212 = nice purple
+    171 = purple
+    141 = cool purple
+    '''
+    colors = {
+        'W' : 15,  # white (normal)
+        'R' : 1, 
+        'G' : 2,
+        'O' : 214,
+        'B' : 4,
+        'P' : 141,
+        'C' : 81,
+        'GR': 245
+    }
 
-
-class Command():
-
-    @staticmethod
-    def devnull():
-        ''' Helper method for opening devnull '''
-        return open('/dev/null', 'w')
+    last_sameline_length = 0
     
     @staticmethod
-    def packer(cmd, pwd):
+    def s(text):
+        base = text
+        output = ''
         
+        matches = re.findall(r"\{([A-Z])\}", text)
+        for match in matches:
+            color_key = match
+            color_code = Color.colors[color_key]
+            base = base.replace("{" + color_key + "}", '')
+            test = stylize(base, fg(color_code))
+        output = ''.join([output, test])
 
-        if type(stdout) is bytes: stdout = stdout.decode('utf-8')
-        if type(stderr) is bytes: stderr = stderr.decode('utf-8')
+        return output
+            
+       
 
-        if Config.verbose > 1 and stdout is not None and stdout.strip() != '':
-            Color.pe('{P} [stdout] %s{W}' % '\n [stdout] '.join(stdout.strip().split('\n')))
-        if Config.verbose > 1 and stderr is not None and stderr.strip() != '':
-            Color.pe('{P} [stderr] %s{W}' % '\n [stderr] '.join(stderr.strip().split('\n')))
-
-        return (stdout, stderr)
-    
     @staticmethod
-    def program_exists(program):
-        ''' Checks if program is installed on this system '''
-        command = ['which', program]
+    def print(text):
+        output = Color.s(text)
+        print(output)    
         
-        proc = Command(command)
+        
+    @staticmethod
+    def set(text, color, attrib=None):
+        r = 1
+        orng = 214
+        y = 220
+        g = 2
+        b = 4
+        cy = 81
+        purp = 141
+        gr = 245
+        wh = 15
 
-        stdout = proc.stdout().strip()
-        stderr = proc.stderr().strip()
+        if color == 'r':
+            color_code = r
 
-        if stdout == '' and stderr == '':
-            return False
+        if color == 'orng':
+            color_code = orng
 
-        return True 
+        if color == 'y':
+            color_code = y
+
+        if color == 'g':
+            color_code = g
+
+        if color == 'b':
+            color_code = b
+
+        if color == 'cy':
+            color_code = cy
+
+        if color == 'purp':
+            color_code = purp
+
+        if color == 'gr':
+            color_code = gr
+
+        if color == 'wh':
+            color_code = wh
 
 
-    def launch(self, path, stdout=s.PIPE, stderr=s.PIPE):
-        ''' Launch a tool from a specific directory'''
-        print('Launching command')
-        proc = s.Popen(self.cmd, cwd=path, stdin=s.PIPE, stdout=stdout, stderr=stderr)
-        out, err = proc.communicate(path)
-        return(out, err)
-
-
-    def __init__(self, cmd='', devnull=False, stdout=s.PIPE, stderr=s.PIPE, cwd=None, bufsize=0, stdin=s.PIPE):
-        '''cmd = command line program with arguments in a list format. Must be in list format to have shell = false'''
-
-        if type(cmd) is str:
-            self.cmd = cmd.split()
+        if attrib is None:
+            output = stylize(text, fg(color_code))
         else:
-            self.cmd = cmd
+            style = fg(color_code) + attr(attrib)
+            output = stylize(text, style)
 
-        self.output = []
-        
-       # self.out = None
-       # self.err = None
+        return output    
 
-       # if devnull:
-       #     std_out = Command.devnull()
-       #     std_err = Command.devnull()
-       # else:
-       #     std_out = stdout
-       #     std_err = stderr
-
-        #self.proc = s.Popen(cmd, stdout=std_out, stderr=std_err, stdin=stdin, cwd=cwd, bufsize=bufsize)      
-
-    def run(self, shell=False, universal_newlines=True):
+    @staticmethod
+    def p(text, color, attrib=None):
         '''
-        Takes in a command prepopulated with arguments and runs it. The command MUST be in a list format'
+        Prints text using colored format on same line.
+        Usage = Color.p('Text.', 'Color Code', Style code)
         '''
-
-        try:
-            self.output = s.check_output(self.cmd, shell=False, stderr=s.STDOUT, universal_newlines=True)
-
-        except s.CalledProcessError as cp:
-            return False
-
-        except OSError as e:
-            return False
-
-        return self
-    
-    
-    def stdout(self):
-        ''' Waits for process to finish, returns stdout output '''
-        self.get_output()
-        return self.out
-
-    def stderr(self):
-        ''' Waits for process to finish, returns stderr output '''
-        self.get_output()
-        return self.err
-
-    def get_output(self):
-        ''' Waits for process to finish, sets stdout & stderr '''
-        if self.out is None:
-            (self.out, self.err) = self.proc.communicate()
-
-        if type(self.out) is bytes:
-            self.out = self.out.decode('utf-8')
-
-        if type(self.err) is bytes:
-            self.err = self.err.decode('utf-8')
-
-        return (self.out, self.err)
+        output = Color.set(text, color, attrib)
+        print(output)
