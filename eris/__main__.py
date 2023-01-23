0e0a0cf3459b1f002ecdc3ab0b81bb17c127d6ab
bdawg <>
2023-01-23 13:38:10 -0700

    fix syntax errors

--- eris/__main__.py
+++ eris/__main__.py
@@ -10,33 +10,40 @@ from eris import __app_name__
 try:
     from .config import Config
 except (ValueError, ImportError) as e:
-    raise Exception('You may need to run wifite from the root directory (which includes README.md)', e)
+    raise Exception(
+        "You may need to run wifite from the root directory (which includes README.md)",
+        e,
+    )
+
 
 class Eris(object):
     def __init__(self):
         from .utils.banner import banner
+
         banner()
 
         Config.init(get_os=True)
-        Color.print('{R}Setting up required configurations...\n')
-         
-#        from .tools.dependency import Dependency
-#        Dependency.dependency_check()
-        
+        Color.print("{R}Setting up required configurations...\n")
+
+    #        from .tools.dependency import Dependency
+    #        Dependency.dependency_check()
+
     def launch(self):
         self.get_module()
         Builder.build(self.pkr_pkgs)
 
     def get_module(self):
-        if Config.module == 'create':
+        if Config.module == "create":
             self.pkr_pkgs = Config.create
-        if Config.module == 'deploy':
-            self.pkr_pkgs = Config.deploy    
-     
+        if Config.module == "deploy":
+            self.pkr_pkgs = Config.deploy
+
+
 ########################################################################
 def run():
     eris = Eris()
     eris.launch()
-    
+
+
 if __name__ == "__main__":
     run()
--- eris/args.py
+++ eris/args.py
@@ -4,11 +4,12 @@ from .utils.colors import Color
 
 import argparse, sys
 
+
 class Arguments(object):
-    ''' Holds arguments used by Eris '''
+    """Holds arguments used by Eris"""
 
     def __init__(self, configuration):
-        self.verbose = '-v' in sys.argv or '-hv' in sys.argv or '-vh' in sys.argv
+        self.verbose = "-v" in sys.argv or "-hv" in sys.argv or "-vh" in sys.argv
         self.config = configuration
         self.args = self.get_arguments()
 
@@ -19,38 +20,62 @@ class Arguments(object):
             return argparse.SUPPRESS
 
     def get_arguments(self):
-        ''' Returns parser.args() containing all program arguments '''
+        """Returns parser.args() containing all program arguments"""
 
-        parser = argparse.ArgumentParser(usage=argparse.SUPPRESS,
-                formatter_class=lambda prog: argparse.HelpFormatter(
-                    prog, max_help_position=80, width=130))
-        
-        parser.add_argument('-v',
-            '--verbose',
-            action='count',
+        parser = argparse.ArgumentParser(
+            usage=argparse.SUPPRESS,
+            formatter_class=lambda prog: argparse.HelpFormatter(
+                prog, max_help_position=80, width=130
+            ),
+        )
+
+        parser.add_argument(
+            "-v",
+            "--verbose",
+            action="count",
             default=0,
-            dest='verbose',
-            help=Color.set('Shows more options','wh'))
-        
+            dest="verbose",
+            help=Color.set("Shows more options", "wh"),
+        )
+
         # Subparser
-        subparse = parser.add_subparsers(dest='module', help="Choose a module to run")
-        
+        subparse = parser.add_subparsers(dest="module", help="Choose a module to run")
+
         # Subparser for Create Module
-        create_parser = subparse.add_parser('create', help='Create pre-configured machine images')
-        create_parser.add_argument('image', action='store', choices=['kali-ami', 'offsec-server', 'phishing-server'], nargs='*', type=str, help='Machine Image you would like to create')
-        
+        create_parser = subparse.add_parser(
+            "create", help="Create pre-configured machine images"
+        )
+        create_parser.add_argument(
+            "image",
+            action="store",
+            choices=["kali-ami", "offsec-server", "phishing-server"],
+            nargs="*",
+            type=str,
+            help="Machine Image you would like to create",
+        )
+
         # deploy subparser
-        deploy_parser = subparse.add_parser('deploy', help='Deploy Operation Offensive Security Infrastructure')
-        deploy_parser.add_argument('infrastructure', action='store', choices=['external-base', 'phishing'], nargs='*', type=str, help='Image you would like to create')
+        deploy_parser = subparse.add_parser(
+            "deploy", help="Deploy Operation Offensive Security Infrastructure"
+        )
+        deploy_parser.add_argument(
+            "infrastructure",
+            action="store",
+            choices=["external-base", "phishing"],
+            nargs="*",
+            type=str,
+            help="Image you would like to create",
+        )
 
         return parser.parse_args()
-    
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     from .utils.colors import Color
     from .config import Config
+
     Config.init(False)
     a = Arguments(Config)
     args = a.args
-    for (key,value) in sorted(args.__dict__.items()):
-        Color.pl('{C}%s: {G}%s{W}' % (key.ljust(21),value))
-    
+    for (key, value) in sorted(args.__dict__.items()):
+        Color.pl("{C}%s: {G}%s{W}" % (key.ljust(21), value))
