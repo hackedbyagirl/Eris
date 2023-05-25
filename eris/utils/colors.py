bc4e8858a5ca2e58cddcf5f7d7a42d253b1b7831
Brette <56555806+hackedbyagirl@users.noreply.github.com>
2023-05-25 14:18:25 -0400

    Update colors.py

--- eris/utils/colors.py
+++ eris/utils/colors.py
@@ -6,74 +6,65 @@ import re
 
 class Color(object):
 
-    """Helpers for printing to terminal"""
+    ''' Helpers for printing to terminal '''
 
-    """
-    Color Keys:
-    red = 1
-    orange = 214
-    yellow = 220
-    green = 2
-    blue = 4
-    cyan = 81
-    purple = 141
-    gray = 245
-    white = 15
-    
-    Attributes keys:
-    0 = reset
-    1 = bold
-    2 = dim
-    21 = reset bold
-    22 = rest dim
-    Extra colors:
-    33 = dog blue
-    226 = bright yellow
-    212 = nice purple
-    171 = purple
-    141 = cool purple
-    """
     colors = {
-        "W": 15,  # white (normal)
-        "R": 1,
-        "G": 2,
-        "O": 214,
-        "B": 4,
-        "P": 141,
-        "C": 81,
-        "GR": 245,
+        'W' : 15,  # white (normal)
+        'R' : 1,
+        'G' : 2,
+        'O' : 214,
+        'B' : 4,
+        'P' : 141,
+        'C' : 81,
+        'GR': 245,
+        'Y' : 226
     }
 
-    last_sameline_length = 0
-
+    '''
+    Prints text using colored format on same line.
+    Example:
+        Color.p('{R}This text is red. {W}This text is white')
+    '''
     @staticmethod
     def s(text):
         base = text
-        output = ""
+        output = ''
+        start = 0
 
-        matches = re.findall(r"\{([A-Z])\}", text)
+        match_pattern = r"\{([A-Z])\}"
+        matches = re.findall(match_pattern, text)
+
         for match in matches:
             color_key = match
             color_code = Color.colors[color_key]
-            base = base.replace("{" + color_key + "}", "")
-            test = stylize(base, fg(color_code))
-        output = "".join([output, test])
+            r_code = "{" + color_key + "}"
+
+            base = base.replace(r_code, '')
+            regex = re.search(match_pattern, base)
+
+            if regex:
+                end = regex.start()
+                substring = base[start:end]
+                start = end
+            else:
+                substring = base[start:]
+
+
+            c_string = Color.color(substring, color_code) + ' '
+            output = ''.join([output, c_string])
 
         return output
+
+    @staticmethod
+    def color(text, code):
+        colored_text = stylize(text, fg(code))
+        return colored_text
 
     @staticmethod
     def print(text):
         output = Color.s(text)
         print(output)
 
-    @staticmethod
-    def p(text, color, attrib=None):
-        """
-        Prints text using colored format on same line.
-        Usage = Color.p('Text.', 'Color Code', Style code)
-        """
-        output = Color.set(text, color, attrib)
-        print(output)
 
     @staticmethod
     def set(text, color, attrib=None):
@@ -87,32 +78,33 @@ class Color(object):
         gr = 245
         wh = 15
 
-        if color == "r":
+        if color == 'r':
             color_code = r
 
-        if color == "orng":
+        if color == 'orng':
             color_code = orng
 
-        if color == "y":
+        if color == 'y':
             color_code = y
 
-        if color == "g":
+        if color == 'g':
             color_code = g
 
-        if color == "b":
+        if color == 'b':
             color_code = b
 
-        if color == "cy":
+        if color == 'cy':
             color_code = cy
 
-        if color == "purp":
+        if color == 'purp':
             color_code = purp
 
-        if color == "gr":
+        if color == 'gr':
             color_code = gr
 
-        if color == "wh":
+        if color == 'wh':
             color_code = wh
+
 
         if attrib is None:
             output = stylize(text, fg(color_code))
@@ -121,3 +113,12 @@ class Color(object):
             output = stylize(text, style)
 
         return output
+
+    @staticmethod
+    def p(text, color, attrib=None):
+        '''
+        Prints text using colored format on same line.
+        Usage = Color.p('Text.', 'Color Code', Style code)
+        '''
+        output = Color.set(text, color, attrib)
+        print(output)
